c00e016ddc2d26ab78ebe0583c7c528c954164e2
Christopher Olivares <christopherdavidolivares@gmail.com>
2014-12-15 16:32:02 -0800

    update workers to use more reliable json parse and tail

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 var Tail        = require("./tail");
-var JSONStream  = require("JSONStream");
+var JSONParse   = require("./jsonparser");
 var async       = require("async");
 var ChildProcess = require("child_process");
 var Readable = require("stream").Readable;
@@ -8,14 +8,13 @@ var Readable = require("stream").Readable;
 var initialized = false;
 process.on("message", function(pipelineFiles) {
   if(!initialized) {
-    var from, through, to;
+    var from, through, to, parser, tail;
     through = require(pipelineFiles.through);
     to = fs.createWriteStream(pipelineFiles.to, {flags: "a"});
     var err = new Error("File might not exist yet");
-    from = ChildProcess.spawn("tail", ["-f", pipelineFiles.from]);
-    var fromStream = new Readable().wrap(from.stdout);
-    var parser = JSONStream.parse();
-    fromStream
+    tail = Tail(pipelineFiles.from);
+    parser = JSONParse();
+    tail.stream
       .pipe(parser)
       .pipe(through)
       .pipe(to);
